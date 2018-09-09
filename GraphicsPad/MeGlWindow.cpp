67180bdbc711b57f82cf4cd3ddf7d690ed3f213d
zhangte0604 <tezhang0604@gmail.com>
2018-09-09 02:25:25 -0600

    WASD for red tri, Arrow key for green tri
    To do: don't need to click mouse to control tris

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -9,12 +9,23 @@ const float Y_DELTA = 0.2f;
 const uint NUM_VERTICES_PER_TRI = 3;
 const uint NUM_FLOATS_PER_VERTICE = 6;
 const uint TIANGLE_BYTE_SIZE = NUM_VERTICES_PER_TRI * NUM_FLOATS_PER_VERTICE * sizeof(float);
-const uint MAX_TRIS = 200;
-const uint MAX_X_SHIFT = 2 / X_DELTA - 1;
-const uint MAX_Y_SHIFT = 2 / Y_DELTA - 1;
+const uint FIRST_MAX_X_SHIFT = 2 / X_DELTA - 1;
+const uint FIRST_MAX_Y_SHIFT = 2 / Y_DELTA - 1;
+
+const uint SEC_MAX_X_SHIFT = 2 / X_DELTA - 1;
+const uint SEC_MAX_Y_SHIFT = 2 / Y_DELTA - 1;
+
+const uint MAX_TRIS = (FIRST_MAX_X_SHIFT+1) * ( FIRST_MAX_Y_SHIFT+1) * (SEC_MAX_X_SHIFT + 1) * (SEC_MAX_Y_SHIFT + 1);
+
 uint numTris = 0;
-uint xShift = 0;
-uint yShift = 0;
+uint numSecTris = 0;
+uint firstXShift = 0;
+uint firstYShift = 0;
+int secXShift = 0;
+int secYShift = 0;
+
+
+
 //extern const char* vertexShaderCode;
 //extern const char* fragmentShaderCode;
 
@@ -42,53 +53,115 @@ void sendDataToOpenGL()
 
 }
 
-void sendAnotherTriToOpenGL()
+void keyboardControl()
 {
+	//WASD controls first tri
 	if (GetKeyState('D') & 0x8000/*check if high-order bit is set (1 << 15)*/)
 	{
 		// do stuff
-		if (xShift < MAX_X_SHIFT) {
-			xShift++;
+		if (firstXShift < FIRST_MAX_X_SHIFT) 
+		{
+			firstXShift++;
 		}
-		//numTris * Y_DELTA
-		//printf("this is a test");
+
 	}
-	else if (GetKeyState('A') & 0x8000) {
-		if (xShift > 0) {
-			xShift--;
-		}
-	}
-	else if (GetKeyState('W') & 0x8000) {
-		if (yShift > 0) {
-			yShift--;
+	else if (GetKeyState('A') & 0x8000) 
+	{
+		if (firstXShift > 0) 
+		{
+			firstXShift--;
 		}
 	}
-	else if (GetKeyState('S') & 0x8000) {
-		if (yShift < MAX_Y_SHIFT) {
-			yShift++;
+	else if (GetKeyState('W') & 0x8000) 
+	{
+		if (firstYShift > 0) 
+		{
+			firstYShift--;
 		}
 	}
-	//initial tri x
-	GLfloat THIS_TRI_X = -1 + xShift * X_DELTA;
-	GLfloat THIS_TRI_Y = 1 - yShift * Y_DELTA;
+	else if (GetKeyState('S') & 0x8000) 
+	{
+		if (firstYShift < FIRST_MAX_Y_SHIFT) 
+		{
+			firstYShift++;
+		}
+	}
+
+	//Arrow keys control second tri
+	if (GetKeyState(VK_RIGHT) & 0x8000/*check if high-order bit is set (1 << 15)*/)
+	{
+		// do stuff
+		if (secXShift * X_DELTA + X_DELTA < 1)
+		{
+			secXShift++;
+		}
+
+	}
+	else if (GetKeyState(VK_LEFT) & 0x8000)
+	{
+		if (secXShift * X_DELTA > -1)
+		{
+			secXShift--;
+			//printf(secXShift);
+		}
+	}
+	else if (GetKeyState(VK_UP) & 0x8000)
+	{
+		if (secYShift > 0)
+		{
+			secYShift--;
+		}
+	}
+	else if (GetKeyState(VK_DOWN) & 0x8000)
+	{
+		if (secYShift < FIRST_MAX_Y_SHIFT)
+		{
+			secYShift++;
+		}
+	}
+}
+void sendAnotherTriToOpenGL()
+{
+
+	keyboardControl();
+
+	//initial tri position
+	GLfloat FIRST_TRI_X = -1 + firstXShift * X_DELTA;
+	GLfloat FIRST_TRI_Y = 1 - firstYShift * Y_DELTA;
+
+	GLfloat SECOND_TRI_X = secXShift * X_DELTA;
+	GLfloat SECOND_TRI_Y = 1 - secYShift * Y_DELTA;
+
+	
 
 	GLfloat thisTri[] =
 	{
-		THIS_TRI_X, THIS_TRI_Y, 0.0f,
+		FIRST_TRI_X, FIRST_TRI_Y, 0.0f,
 		1.0f, 0.0f, 0.0f,
 
-		THIS_TRI_X + X_DELTA, THIS_TRI_Y, 0.0f,
+		FIRST_TRI_X + X_DELTA, FIRST_TRI_Y, 0.0f,
 		1.0f, 0.0f, 0.0f,
 
-		THIS_TRI_X, THIS_TRI_Y - Y_DELTA, 0.0f,
+		FIRST_TRI_X, FIRST_TRI_Y - Y_DELTA, 0.0f,
 		1.0f, 0.0f, 0.0f,
+
+
+		SECOND_TRI_X, SECOND_TRI_Y, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		SECOND_TRI_X + X_DELTA, SECOND_TRI_Y, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		SECOND_TRI_X, SECOND_TRI_Y - Y_DELTA, 0.0f,
+		0.0f, 1.0f, 0.0f,
 
 	};
 
+	
 	//send data to a sub buffer, not entire buffer
 	glBufferSubData(GL_ARRAY_BUFFER,
-		numTris * TIANGLE_BYTE_SIZE, TIANGLE_BYTE_SIZE, thisTri);
-	numTris++;
+		numTris * TIANGLE_BYTE_SIZE, TIANGLE_BYTE_SIZE * 2, thisTri);
+	numTris+=2;
 }
 
 //run everytime you call
@@ -105,8 +178,9 @@ void MeGLWindow::paintGL()
 	glViewport(0, 0, width(), height());
 	
 	sendAnotherTriToOpenGL();
+	
 	//draw tris
-	glDrawArrays(GL_TRIANGLES, (numTris-1) * NUM_VERTICES_PER_TRI,  NUM_VERTICES_PER_TRI);
+	glDrawArrays(GL_TRIANGLES, (numTris-2) * NUM_VERTICES_PER_TRI,  NUM_VERTICES_PER_TRI*2);
 
 }
 
