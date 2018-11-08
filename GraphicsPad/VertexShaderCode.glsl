d62a066581cca9a44dc8fd09cdf97061190a007d
zhangte0604 <tezhang0604@gmail.com>
2018-11-08 13:42:32 -0700

    vid 93: VertexShaderCode Refactor

--- GraphicsPad/VertexShaderCode.glsl
+++ GraphicsPad/VertexShaderCode.glsl
@@ -1,7 +1,7 @@
 
 #version 430
 
-in layout(location = 0) vec3 vertexPositionModel;
+in layout(location = 0) vec4 vertexPositionModel;
 in layout(location = 1) vec3 vertexColor;
 in layout(location = 2) vec3 normalModel;
 
@@ -15,10 +15,10 @@ out vec3 vertexPositionWorld;
 
 void main()
 {
-	vec4 v = vec4(vertexPositionModel, 1.0);
-	gl_Position = modelToProjectionMatrix * v;
+	//vec4 v = vec4(vertexPositionModel, 1.0);
+	gl_Position = modelToProjectionMatrix * vertexPositionModel;
 	normalWorld = vec3(modelToWorldMatrix * vec4(normalModel, 0));
-	vertexPositionWorld = vec3(modelToWorldMatrix * v);
+	vertexPositionWorld = vec3(modelToWorldMatrix * vertexPositionModel);
 	
 }
 
