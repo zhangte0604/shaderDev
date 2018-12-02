d052141b9afb0cbbd3be827e5614a905549dfc9c
zhangte0604 <tezhang0604@gmail.com>
2018-12-01 17:35:29 -0700

    Working on vertex shader

--- GraphicsPad/TextureVertexShaderCode.glsl
+++ GraphicsPad/TextureVertexShaderCode.glsl
@@ -4,16 +4,17 @@
 in layout(location = 0) vec4 vertexPositionModel;
 in layout(location = 1) vec3 vertexColor;
 in layout(location = 2) vec3 normalModel;
-in layout(location = 3) vec2 VertexTexCoord;
-in layout(location = 4) vec3 tangentModel;
+in layout(location = 3) vec2 vertexTexCoord;
+in layout(location = 4) vec3 vertexTangent;
 
 uniform mat4 modelToProjectionMatrix;
 uniform mat4 modelToWorldMatrix;
+uniform mat4 normalMatrix;
 
 out vec3 vertexPositionWorld;
 out vec3 normalWorld;
-out vec2 TexCoord;
-out mat4 tangentToModelTransform;
+out vec2 texCoord;
+//out mat4 tangentToModelTransform;
 
 
 void main()
@@ -28,6 +29,21 @@ void main()
 	vertexPositionWorld = vec3(modelToWorldMatrix * vertexPositionModel);
 
 	TexCoord = VertexTexCoord;
+
+	//Transform normal and tangent to eye space
+	vec3 norm = normalize(normalMatrix * normalModel);
+	vec3 tang = normalize(normalMatrix * vertexTangent);
+	//Compute the binormal
+	vec3 binormal = normalize(cross(norm, tang)) * 1;
+
+	//Matrix for transformation to tangent space
+	mat3 toObjectLocal = mat3(
+		tang.x, binormal.x, norm.x,
+		tang.y, binormal.y, norm.y,
+		tang.z, binormal.z, norm.z);
+
+	//
+
 	
 	//vec3 bitangentVector = normalize(cross(normalModel, tangentModel));
 	//tangentToModelTransform = mat4(vec4(tangentModel, 0.0), vec4(bitangentVector, 0.0), vec4(normalModel, 0.0), vec4(vertexPositionModel, 1.0));
